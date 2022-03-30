e62811290a75df9f3acdf696562ecdc77ef831d4
WLUO <58965905+wolfatthegate@users.noreply.github.com>
2022-03-30 15:49:32 -0400

    Update 4041-N-th-Tribonacci-2.py

--- 4041-N-th-Tribonacci-2.py
+++ 4041-N-th-Tribonacci-2.py
@@ -1,12 +1,12 @@
-/* This is a top-down implementation with Recursive function
+''' 
+This is a top-down implementation with Recursive function
 
 The Tribonacci sequence Tn is defined as follows: 
 
 T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 
 Given n, return the value of Tn. 
-
-*/
+'''
 
 class Solution:
     def tribonacci(self, n: int) -> int:
